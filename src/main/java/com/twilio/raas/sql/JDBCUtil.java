22d23e801d49a64de50843800d5c4663003df6b5
Thomas Dsilva <tdsilva@twilio.com>
2020-04-15 13:49:35 -0700

    RC-1444 Unify dataset avro definition and move all datasets to a single location (#200)

--- src/main/java/com/twilio/raas/sql/JDBCUtil.java
+++ src/main/java/com/twilio/raas/sql/JDBCUtil.java
@@ -1,18 +1,31 @@
 package com.twilio.raas.sql;
 
+import com.google.common.base.Joiner;
+import com.twilio.dataset.DatasetUtil;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class JDBCUtil {
 
-  public static final String DESCENDING_COLUMNS = "kuduTableConfigs:[" +
-      "{tableName: 'ReportCenter.AuditEvents', descendingSortedFields:['event_date']}, " +
-      "{tableName: 'AuditEvents-DailyIndex-Aggregation', descendingSortedFields:['event_date']}," +
-      "{tableName: 'ReportCenter.MsgInsights', descendingSortedFields:['date_created']}, " +
-      "{tableName: 'MsgInsights-Carrier-Aggregation', descendingSortedFields:['date_created']}, " +
-      "{tableName: 'MsgInsights-Number-Aggregation', descendingSortedFields:['date_created']}" +
-      "]}";
+  public static final String DESCENDING_COLUMNS = getDescendingColumns();
 
   public static String CALCITE_MODEL_TEMPLATE = "jdbc:calcite:model=inline:{version: '1.0'," +
     "defaultSchema:'kudu',schemas:[{name: 'kudu',type:'custom'," +
     "factory:'com.twilio.raas.sql.KuduSchemaFactory',operand:{connect:'%s'," + DESCENDING_COLUMNS +
     "}]};caseSensitive=false;timeZone=UTC";
+
+  private static String getDescendingColumns() {
+    StringBuilder sb = new StringBuilder("kuduTableConfigs:[");
+    List <String> descendingList = DatasetUtil.INSTANCE.getDescendingDateFields()
+      .stream()
+      .map(
+        descendingInfo -> String.format("{tableName: '%s', descendingSortedFields:['%s']}",
+          descendingInfo.tableName, descendingInfo.columnName))
+      .collect(Collectors.toList());
+    sb.append(Joiner.on(", ").join(descendingList));
+    sb.append("]}");
+    return sb.toString();
+  }
 
 }
