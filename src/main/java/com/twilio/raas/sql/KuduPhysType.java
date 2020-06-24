8e747728146cb9f78dcdc68dcce7a9222fd89037
Scott Reynolds <sreynolds@twilio.com>
2020-06-23 21:01:17 -0700

    Implement a few methods of KuduPhysType
    Summary:
    Should give us a bit of protection in case we do something more with
    this class. throwing UnsupportedOperationException is better then a
    null pointer exception.

--- src/main/java/com/twilio/raas/sql/KuduPhysType.java
+++ src/main/java/com/twilio/raas/sql/KuduPhysType.java
@@ -2,13 +2,16 @@ package com.twilio.raas.sql;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.math.BigDecimal;
 import java.nio.ByteBuffer;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 
+import org.apache.calcite.adapter.enumerable.EnumUtils;
 import org.apache.calcite.adapter.enumerable.JavaRowFormat;
 import org.apache.calcite.adapter.enumerable.PhysType;
+import org.apache.calcite.linq4j.function.Function1;
 import org.apache.calcite.linq4j.tree.Expression;
 import org.apache.calcite.linq4j.tree.Expressions;
 import org.apache.calcite.linq4j.tree.ParameterExpression;
@@ -16,6 +19,7 @@ import org.apache.calcite.linq4j.tree.Types;
 import org.apache.calcite.rel.RelCollation;
 import org.apache.calcite.rel.RelFieldCollation;
 import org.apache.calcite.rel.type.RelDataType;
+import org.apache.calcite.util.BuiltInMethod;
 import org.apache.calcite.util.Pair;
 import org.apache.kudu.ColumnSchema;
 import org.apache.kudu.Schema;
@@ -56,41 +60,60 @@ public final class KuduPhysType implements PhysType {
         return RowResult.class;
     }
     @Override
-    public Type getJavaFieldType(int field) {
-        // TODO Auto-generated method stub
-        return null;
+    public Type getJavaFieldType(final int field) {
+        return fieldClass(field);
     }
     @Override
-    public PhysType field(int ordinal) {
-        // TODO Auto-generated method stub
-        return null;
+    public PhysType field(final int ordinal) {
+        throw new UnsupportedOperationException();
     }
     @Override
-    public PhysType component(int field) {
-        // TODO Auto-generated method stub
-        return null;
+    public PhysType component(final int field) {
+        throw new UnsupportedOperationException();
     }
     @Override
     public RelDataType getRowType() {
         return logicalType;
     }
     @Override
-    public Class fieldClass(int field) {
-        // TODO Auto-generated method stub
-        return null;
+    public Class fieldClass(final int field) {
+        final ColumnSchema columnSchema = tableSchema.getColumns().get(field);
+        switch(columnSchema.getType()) {
+        case INT8:
+            return Byte.class;
+        case INT16:
+            return Short.class;
+        case INT32:
+            return Integer.class;
+        case INT64:
+        case UNIXTIME_MICROS:
+            return Long.class;
+        case FLOAT:
+            return Float.class;
+
+        case DOUBLE:
+            return Double.class;
+        case DECIMAL:
+            return BigDecimal.class;
+
+        case STRING:
+            return String.class;
+        case BOOL:
+            return Boolean.class;
+
+        case BINARY:
+            return byte[].class;
+        default:
+            throw new IllegalArgumentException("Unable to do the thing " + columnSchema.getType());
+        }
     }
     @Override
-    public boolean fieldNullable(int index) {
-        // TODO Auto-generated method stub
-        return false;
+    public boolean fieldNullable(final int index) {
+        final ColumnSchema columnSchema = tableSchema.getColumns().get(index);
+        return columnSchema.isNullable();
     }
     @Override
-    public Expression fieldReference(Expression expression, int field) {
-        // TODO Auto-generated method stub
-        return fieldReference(expression, field, null);
-    }
-    @Override
-    public Expression fieldReference(Expression expression, int ord, Type storageType) {
+    public Expression fieldReference(final Expression expression, final int ord) {
         final int field = this.kuduColumnProjections.indexOf(ord);
         final ColumnSchema columnSchema = tableSchema.getColumns().get(ord);
         Expression descendingSortMax = null;
@@ -157,89 +180,175 @@ public final class KuduPhysType implements PhysType {
         else {
             return rawFetch;
         }
+    }
+    @Override
+    public Expression fieldReference(final Expression expression, final int ord, final Type storageType) {
+        // @TODO: This probably should leverage storageType similar to
+        // {@link org.apache.calcite.rex.RexLiteral#getValueAs(Class<T>)}
+        return fieldReference(expression, ord);
+    }
+    @Override
+    public Expression generateAccessor(final List<Integer> fields) {
+        // Mostly Copy an pasta
+        final ParameterExpression v1 =
+        Expressions.parameter(getJavaRowType(), "v1");
+        switch (fields.size()) {
+        case 0:
+            return Expressions.lambda(
+                Function1.class,
+                Expressions.field(
+                    null,
+                    BuiltInMethod.COMPARABLE_EMPTY_LIST.field),
+          v1);
+        case 1:
+            final int field0 = fields.get(0);
 
+            // new Function1<Employee, Res> {
+            //    public Res apply(Employee v1) {
+            //        return v1.<fieldN>;
+            //    }
+            // }
+            final Class returnType = fieldClass(field0);
+            final Expression fieldReference =
+                EnumUtils.convert(
+                    fieldReference(v1, field0),
+                    returnType);
+            return Expressions.lambda(
+                Function1.class,
+                fieldReference,
+                v1);
+        default:
+            // new Function1<Employee, List> {
+            //    public List apply(Employee v1) {
+            //        return Arrays.asList(
+            //            new Object[] {v1.<fieldN>, v1.<fieldM>});
+            //    }
+            // }
+            final Expressions.FluentList<Expression> list = Expressions.list();
+            for (final int field : fields) {
+                list.add(fieldReference(v1, field));
+            }
+            switch (list.size()) {
+            case 2:
+                return Expressions.lambda(
+                    Function1.class,
+                    Expressions.call(
+                        List.class,
+                        null,
+                        BuiltInMethod.LIST2.method,
+                        list),
+                    v1);
+            case 3:
+                return Expressions.lambda(
+                    Function1.class,
+                    Expressions.call(
+                        List.class,
+                        null,
+                        BuiltInMethod.LIST3.method,
+                  list),
+                    v1);
+            case 4:
+                return Expressions.lambda(
+                    Function1.class,
+                    Expressions.call(
+                        List.class,
+                        null,
+                        BuiltInMethod.LIST4.method,
+                        list),
+                    v1);
+            case 5:
+                return Expressions.lambda(
+                    Function1.class,
+                    Expressions.call(
+                        List.class,
+                        null,
+                        BuiltInMethod.LIST5.method,
+                        list),
+                    v1);
+            case 6:
+                return Expressions.lambda(
+                    Function1.class,
+                    Expressions.call(
+                        List.class,
+                        null,
+                        BuiltInMethod.LIST6.method,
+                        list),
+                    v1);
+            default:
+                return Expressions.lambda(
+                    Function1.class,
+                    Expressions.call(
+                        List.class,
+                        null,
+                        BuiltInMethod.LIST_N.method,
+                        Expressions.newArrayInit(
+                            Comparable.class,
+                            list)),
+                    v1);
+            }
+        }
     }
     @Override
-    public Expression generateAccessor(List<Integer> fields) {
-        // TODO Auto-generated method stub
-        return null;
+    public Expression generateSelector(final ParameterExpression parameter, final List<Integer> fields) {
+        throw new UnsupportedOperationException();
     }
     @Override
-    public Expression generateSelector(ParameterExpression parameter, List<Integer> fields) {
-        // TODO Auto-generated method stub
-        return null;
+    public Expression generateSelector(final ParameterExpression parameter, final List<Integer> fields,
+        final JavaRowFormat targetFormat) {
+        return generateSelector(parameter, fields);
     }
     @Override
-    public Expression generateSelector(ParameterExpression parameter, List<Integer> fields,
-        JavaRowFormat targetFormat) {
-        // TODO Auto-generated method stub
-        return null;
+    public Expression generateSelector(final ParameterExpression parameter, final List<Integer> fields, final List<Integer> usedFields,
+        final JavaRowFormat targetFormat) {
+        return generateSelector(parameter, fields);
     }
     @Override
-    public Expression generateSelector(ParameterExpression parameter, List<Integer> fields, List<Integer> usedFields,
-        JavaRowFormat targetFormat) {
-        // TODO Auto-generated method stub
-        return null;
+    public Pair<Type, List<Expression>> selector(final ParameterExpression parameter, final List<Integer> fields,
+        final JavaRowFormat targetFormat) {
+        throw new UnsupportedOperationException();
     }
     @Override
-    public Pair<Type, List<Expression>> selector(ParameterExpression parameter, List<Integer> fields,
-        JavaRowFormat targetFormat) {
-        // TODO Auto-generated method stub
-        return null;
+    public PhysType project(final List<Integer> integers, final JavaRowFormat format) {
+        throw new UnsupportedOperationException();
     }
     @Override
-    public PhysType project(List<Integer> integers, JavaRowFormat format) {
-        // TODO Auto-generated method stub
-        return null;
+    public PhysType project(final List<Integer> integers, final boolean indicator, final JavaRowFormat format) {
+        return project(integers, format);
     }
     @Override
-    public PhysType project(List<Integer> integers, boolean indicator, JavaRowFormat format) {
-        // TODO Auto-generated method stub
-        return null;
+    public Pair<Expression, Expression> generateCollationKey(final List<RelFieldCollation> collations) {
+        throw new UnsupportedOperationException();
     }
     @Override
-    public Pair<Expression, Expression> generateCollationKey(List<RelFieldCollation> collations) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    @Override
-    public Expression generateComparator(RelCollation collation) {
-        // TODO Auto-generated method stub
-        return null;
+    public Expression generateComparator(final RelCollation collation) {
+        throw new UnsupportedOperationException();
     }
     @Override
     public Expression comparer() {
-        // TODO Auto-generated method stub
-        return null;
+        throw new UnsupportedOperationException();
     }
     @Override
-    public Expression record(List<Expression> expressions) {
-        // TODO Auto-generated method stub
-        return null;
+    public Expression record(final List<Expression> expressions) {
+        throw new UnsupportedOperationException();
     }
     @Override
     public JavaRowFormat getFormat() {
-        // TODO Auto-generated method stub
-        return null;
+        throw new UnsupportedOperationException();
     }
     @Override
-    public List<Expression> accessors(Expression parameter, List<Integer> argList) {
-        // TODO Auto-generated method stub
-        return null;
+    public List<Expression> accessors(final Expression parameter, final List<Integer> argList) {
+        throw new UnsupportedOperationException();
     }
     @Override
-    public PhysType makeNullable(boolean nullable) {
-        // TODO Auto-generated method stub
-        return null;
+    public PhysType makeNullable(final boolean nullable) {
+        throw new UnsupportedOperationException();
     }
     @Override
-    public Expression convertTo(Expression expression, PhysType targetPhysType) {
-        // TODO Auto-generated method stub
-        return null;
+    public Expression convertTo(final Expression expression, final PhysType targetPhysType) {
+        throw new UnsupportedOperationException();
     }
     @Override
-    public Expression convertTo(Expression expression, JavaRowFormat targetFormat) {
-        // TODO Auto-generated method stub
-        return null;
+    public Expression convertTo(final Expression expression, final JavaRowFormat targetFormat) {
+        throw new UnsupportedOperationException();
     }
 }
