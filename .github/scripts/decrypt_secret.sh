dadf3ea3fb0193d9628a1681572bd010b73270f5
Scott Reynolds <sreynolds@twilio.com>
2021-01-05 12:45:16 -0800

    Enclose Password in double quotes
    Summary:
    this matches the github actions flow properly

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,6 +4,5 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt  --passphrase=$TARBALL_PASSWORD \
-    --output travis/local.secrets.tar \
-    travis/actions-secrets.tar.gpg
+gpg --quiet --batch --yes --decrypt  --passphrase="$TARBALL_PASSWORD" \
+    --output travis/local.secrets.tar travis/actions-secrets.tar.gpg
