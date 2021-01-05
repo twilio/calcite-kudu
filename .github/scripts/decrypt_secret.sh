a5905054b1419ac2a384b8f6115f40b971401d5e
Scott Reynolds <sreynolds@twilio.com>
2021-01-05 12:50:15 -0800

    Unpack the files so the secret is setup for maven

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -6,3 +6,5 @@ mkdir $HOME/secrets
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt  --passphrase="$TARBALL_PASSWORD" \
     --output travis/local.secrets.tar travis/actions-secrets.tar.gpg
+# untar the files
+tar xv -C travis -f travis/local.secrets.tar
