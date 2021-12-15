6826b92f820976036dc066ef280be41b70554aff
Thomas D'Silva <twdsilva@gmail.com>
2021-12-15 14:00:04 -0800

    Update adapter/src/main/java/com/twilio/kudu/dataloader/generator/ColumnValueGenerator.java
    Co-authored-by: Scott Reynolds <36394400+sdreynolds@users.noreply.github.com>

--- adapter/src/main/java/com/twilio/kudu/dataloader/generator/ColumnValueGenerator.java
+++ adapter/src/main/java/com/twilio/kudu/dataloader/generator/ColumnValueGenerator.java
@@ -24,9 +24,9 @@ public abstract class ColumnValueGenerator {
 
   public abstract Object getColumnValue(String columnName);
 
-  // needed because while reading from json the default constructor is used
-  // without member
-  // variables being initialized first
+  /** 
+    * Initialize the {@code ColumnValueGenerator} from JSON or other files. Used as a alternative constructor
+    */
   public abstract void initialize();
 
 }
