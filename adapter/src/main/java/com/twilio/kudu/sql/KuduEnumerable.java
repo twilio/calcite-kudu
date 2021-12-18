cd77c476dc5696ea59ca93025f5bd55472f6e5c2
mkorad <mkorad@twilio.com>
2021-12-17 17:17:37 -0800

    mvn spotless apply

--- adapter/src/main/java/com/twilio/kudu/sql/KuduEnumerable.java
+++ adapter/src/main/java/com/twilio/kudu/sql/KuduEnumerable.java
@@ -522,7 +522,7 @@ public final class KuduEnumerable extends AbstractEnumerable<Object> implements 
       // Allows for consistent row order in reads as it puts in ORDERED by Pk when
       // faultTolerant is set to true
       // setFaultTolerant to true sets the ReadMode to READ_AT_SNAPSHOT
-      //setting snapshot time to 1ms before currentTimestamp to avoid latency issues.
+      // setting snapshot time to 1ms before currentTimestamp to avoid latency issues.
       tokenBuilder.snapshotTimestampMicros(System.currentTimeMillis() * 1000 - 1000);
       tokenBuilder.setFaultTolerant(true);
       tokenBuilder.replicaSelection(ReplicaSelection.CLOSEST_REPLICA);
